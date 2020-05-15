59154b025cb6a50048ebd4e51af7d44b3f5080aa
Ibrahim Luqman <ibm.jnk@yandex.com>
2020-05-16 00:06:00 +0300

    fix site settings

--- site_settings/forms.py
+++ site_settings/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django_countries.fields import CountryField
 from django.core import validators
 import re
+from .models import CompanySettings
+
+company_data = CompanySettings.objects.get(slug='company')
 
 REGEX_PATTERS = {
     "NAME": re.compile(r"^[a-zA-Z '.-]*$", re.IGNORECASE),  # name or first second
@@ -19,8 +22,8 @@ class CompanySettingsForm(forms.Form):
     logo = forms.FileField(label='Logo', required=False)
     url = forms.URLField(label="Website URL", required=False, max_length=100,
                          validators=[validators.URLValidator(message="Website URL not correct!")])
-    description = forms.CharField(label="Description", required=False, widget=forms.Textarea)
-    keywords = forms.CharField(label="Keywords", required=False, widget=forms.Textarea)
+    description = forms.CharField(label="Description", required=False, widget=forms.Textarea, initial=company_data.description)
+    keywords = forms.CharField(label="Keywords", required=False, widget=forms.Textarea, initial=company_data.keywords)
     country = CountryField(blank_label='Select Country').formfield(label="Country", required=False)
     city = forms.CharField(label="City", max_length=20, required=False, validators=[
         validators.RegexValidator(regex=REGEX_PATTERS['NAME'], message="City must be Alphabet!")])
@@ -29,6 +32,6 @@ class CompanySettingsForm(forms.Form):
     admin_email = forms.EmailField(label="Admin Email", max_length=100, required=False,
                                    validators=[validators.EmailValidator(message="Admin Email Not Valid!")])
     status = forms.BooleanField(label="Website in Maintenance mode:", required=False)
-    closed_msg = forms.CharField(label="Closed Message", required=False, widget=forms.Textarea)
+    closed_msg = forms.CharField(label="Closed Message", required=False, widget=forms.Textarea, initial=company_data.close_msg)
 
     
