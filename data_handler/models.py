6ca9ab79d9705cb532e1824a030844f27cd7bbb8
Ibrahim Luqman <ibm.jnk@yandex.com>
2020-05-17 20:31:58 +0300

    Data handler Model

--- data_handler/models.py
+++ data_handler/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from membership.models import UserMembership
 # Create your models here.
+
+
+class MemberData(models.Model):
+    member = models.OneToOneField(UserMembership, null=True, blank=True, on_delete=models.CASCADE)
+    # file will be saved to MEDIA_ROOT/uploads/2015/01/30
+    data_file_path = models.FileField(upload_to="members_data/%Y/%m/%d/", null=True, blank=True)
+    all_records_count = models.BigIntegerField(null=True, blank=True)
+    allowed_records_count = models.IntegerField(null=True, blank=True)
+    uploaded_date = models.DateTimeField(auto_now_add=True)
+
+
