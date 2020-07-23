04607cd8f883e185da71273e8c62afe3f5e35eb2
Ibrahim Luqman <ibm.jnk@yandex.com>
2020-07-23 19:04:00 +0300

    fix tooltip for valid columns select menu

--- predict_me/static/assets/js/custom/datahandler/helpers.js
+++ predict_me/static/assets/js/custom/datahandler/helpers.js
@@ -983,7 +983,7 @@ function reselectColumnsFunc() {
                         class='pickedItem list-group-item d-flex justify-content-between align-items-center cursor-pointer list-group-item-action'>
                         ${name}
                         <span class="nav-label mx-10" >
-                            <select data-value='${getDataType(dType)}' class="form-control form-control-sm h-40px column-option-dtype" title="NO TOOLTIP HERE">
+                            <select data-value='${getDataType(dType)}' class="form-control form-control-sm h-40px column-option-dtype">
                                     ${dataTypeOptions(data['selected_columns'][name], true, data['unique_column'], name)}
                             </select>
                         </span>
