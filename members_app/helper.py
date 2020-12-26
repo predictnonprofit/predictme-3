c2ca276e852257cd5d629ec41742751e084fccd2
Ibrahim Luqman <ibm.jnk@yandex.com>
2020-12-25 20:33:03 -0500

    helpers file to members_app

---
+++ members_app/helper.py
@@ -0,0 +1,10 @@
+def calculate_records_left_percentage(data_obj, data_session):
+    """
+    this function will take data object, and data session to calculate how many left of records used
+    by get the percentage of the records that left and not used.
+    @return this will return integer
+    """
+    allowed_records_count = data_obj.allowed_records_count
+    used_records = data_session.all_records_count
+    total_used = used_records / allowed_records_count
+    return round(total_used * 100)
