856d63e6da939bcd287ab74450584456f521f7ee
Ibrahim Luqman <ibm.jnk@yandex.com>
2021-01-04 19:42:39 -0500

    subscription now compatible with new version of stripe APIs

--- membership/views.py
+++ membership/views.py
@@ -11,6 +11,7 @@ import stripe
 import os
 import datetime
 import pytz
+from django.db.models import Q
 
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
@@ -27,17 +28,22 @@ class CheckoutView(View):
     def post(self, request):
         if request.method == "POST":
             member = request.user
-            cprint(request.POST, "red")
+            # cprint(request.POST, "cyan")
             sub_range = request.POST.get("sub_range")
             payment_agree = request.POST.get("agree_payment")
             stripe_token = request.POST.get("stripeToken")
             member.stripe_card_token = stripe_token
             member.status = "active"
             member.save()
-            #id, created, plan[interval], plan[product], price[active], latest_invoice, status, current_period_end
+            # id, created, plan[interval], plan[product], price[active], latest_invoice, status, current_period_end
             # current_period_start, customer
             subscription = Subscription.objects.get(member_id=member)
-
+            # cprint(subscription.stripe_plan_id, 'blue')
+            membership = Membership.objects.filter(
+                Q(range_label=sub_range) & Q(parent=subscription.stripe_plan_id.slug)).first()
+            subscription.stripe_plan_id = membership
+            subscription.save()
+            # cprint(subscription.stripe_plan_id, 'magenta')
             customer = stripe.Customer.modify(
                 subscription.member_id.stripe_customer_id,
                 card=stripe_token
@@ -47,7 +53,11 @@ class CheckoutView(View):
             stripe_subscription_obj = stripe.Subscription.create(
                 customer=customer['id'],
                 items=[
-                    {"price": subscription.stripe_plan_id.stripe_plane_id},
+                    {"price": subscription.stripe_plan_id.stripe_price_id},
+                    # {"price_data": {
+                    #     "product": subscription.stripe_plan_id.stripe_plane_id,
+                    #     "currency": "usd"
+                    # }},
                 ],
             )
             tmp_customer = stripe.Customer.retrieve(customer['id'])
@@ -55,8 +65,10 @@ class CheckoutView(View):
             subscription.stripe_subscription_id = stripe_subscription_obj['id']
             subscription.stripe_card_id = card_id
             card_obj = stripe.Customer.retrieve_source(customer['id'], card_id)
-            start_date = datetime.datetime.fromtimestamp(stripe_subscription_obj['current_period_start'], tz=pytz.UTC).strftime("%Y-%m-%d %H:%M:%S")
-            end_date = datetime.datetime.fromtimestamp(stripe_subscription_obj['current_period_end'], tz=pytz.UTC).strftime("%Y-%m-%d %H:%M:%S")
+            start_date = datetime.datetime.fromtimestamp(stripe_subscription_obj['current_period_start'],
+                                                         tz=pytz.UTC).strftime("%Y-%m-%d %H:%M:%S")
+            end_date = datetime.datetime.fromtimestamp(stripe_subscription_obj['current_period_end'],
+                                                       tz=pytz.UTC).strftime("%Y-%m-%d %H:%M:%S")
             subscription.subscription_status = True if stripe_subscription_obj['status'] == "active" else False
             subscription.subscription_period_start = start_date
             subscription.subscription_period_end = end_date
@@ -64,18 +76,11 @@ class CheckoutView(View):
             subscription.card_brand = card_obj['brand']
             subscription.card_expire = datetime.datetime(card_obj['exp_year'], card_obj['exp_month'], 1)
             subscription.sub_range = sub_range
-            # cprint(stripe_subscription_obj, "yellow")
+
             subscription.save()
-            # sub_start_date = datetime.now()
-            # sub_end_date = sub_start_date + relativedelta(months=+1)
-            # subscription = Subscription(member=member, stripe_subscription_id=new_member_subscription_stripe_id['id'],
-            #                               active=True, sub_range=sub_range, subscription_start_date=sub_start_date,
-            #                               subscription_end_date=sub_end_date)
-            #
-            # print(f"Subscription done: {new_member_subscription_stripe_id}")
-            # print(new_member_stripe_id['id'])
-            # return HttpResponse(new_member_stripe_id['id'])
+
             return redirect(reverse("register_successfully"))
+            # return JsonResponse(data={"status": "register Done"}, status=200)
 
 
 class RegisterSuccessfully(TemplateView):
